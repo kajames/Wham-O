c3d096b25eadfaa541d75fb8e6e63c756e8afe66
kajames <kajames@gmail.com>
2013-03-18 11:19:11 -0400

    Lots of comments a a few very minor code cleanups

--- FRC2994_2013/FRC2994_2013.cpp
+++ FRC2994_2013/FRC2994_2013.cpp
@@ -14,8 +14,8 @@
 class RobotDemo : public SimpleRobot
 {
 	// Drive motors
-	LEFT_DRIVE_MOTOR leftFrontDriveMotor; // Done BEFORE the robot drive which uses them
-	LEFT_DRIVE_MOTOR leftRearDriveMotor;
+	LEFT_DRIVE_MOTOR  leftFrontDriveMotor; // Done BEFORE the robot drive which uses them
+	LEFT_DRIVE_MOTOR  leftRearDriveMotor;
 	RIGHT_DRIVE_MOTOR rightFrontDriveMotor;
 	RIGHT_DRIVE_MOTOR rightRearDriveMotor;
 	
@@ -53,9 +53,9 @@ class RobotDemo : public SimpleRobot
 	Timer shooterTimer;
 
 	// Nonobject members
-	bool m_shooterMotorRunning;
-	bool m_jogTimerRunning;
-	int  m_shiftCount;
+	bool  m_shooterMotorRunning;
+	bool  m_jogTimerRunning;
+	int   m_shiftCount;
 	float m_previousPot;
 	
 	DriverStationLCD *dsLCD;
@@ -66,8 +66,8 @@ public:
 		leftRearDriveMotor(LEFT_REAR_DRIVE_PWM),
 		rightFrontDriveMotor(RIGHT_FRONT_DRIVE_PWM),
 		rightRearDriveMotor(RIGHT_REAR_DRIVE_PWM),
-		myRobot(&leftFrontDriveMotor, &leftRearDriveMotor, &rightFrontDriveMotor, &rightRearDriveMotor),	// these must be initialized in the same order
-		stick(1),									// as they are declared above.
+		myRobot(&leftFrontDriveMotor, &leftRearDriveMotor, &rightFrontDriveMotor, &rightRearDriveMotor),
+		stick(1),
 		stick2(2),
 		gamepad(3),
 		indexerMotor(INDEX_PWM),
@@ -86,9 +86,9 @@ public:
 		jogTimer(),
 		shooterTimer()
 	{
-		m_shooterMotorRunning   = false;
-		m_jogTimerRunning       = false;
-		m_shiftCount            = MAX_SHIFTS;
+		m_shooterMotorRunning = false;
+		m_jogTimerRunning     = false;
+		m_shiftCount          = MAX_SHIFTS;
 		
 		dsLCD = DriverStationLCD::GetInstance();
 		dsLCD->Clear();
@@ -108,7 +108,6 @@ public:
 		leftDriveEncoder.SetMaxPeriod(1.0);
 		leftDriveEncoder.SetReverseDirection(true);  // change to true if necessary
 		leftDriveEncoder.Start();
-
 	}
 	
 	double absolute(double value)
@@ -120,41 +119,47 @@ public:
 		return value;
 	}
 	
+	// Robot movements in autonomous mode are table driven. Each entry of
+	// the tables is executed by this method.
 	void DoAutonomousMoveStep(const step_speed *speeds, char * message)
 	{
+		// There are encoders on both sides of the drive system. We only use
+		// one for simplicity.
 		leftDriveEncoder.Reset();
 		double dist = speeds[0].distance;
 		double reading;
 		
 		// Start moving the robot
 		myRobot.Drive(speeds->magnitude, speeds->curve);
-//		leftFrontDriveMotor.Set(speeds->speed_left);
-//		rightFrontDriveMotor.Set(speeds->speed_right);
 
 		reading = absolute(leftDriveEncoder.GetDistance());
 		
-		while (dist > reading)
+		// Just in case something goes wrong with the encoder (which could
+		// leave us in an infinite loop and prevent us from entering telop
+		// mode) we also exit the loop when no longer in autonomous mode.
+		while (IsAutonomous() && (dist > reading))
 		{
 			Wait(0.02);
 			reading = absolute(leftDriveEncoder.GetDistance());
 			dsLCD->UpdateLCD();
 		}
 		
-		leftFrontDriveMotor.Set(0.0);
-		rightFrontDriveMotor.Set(0.0);
+		myRobot.Drive(0.0, 0.0);
 	}
 
 	// Shoot a single disk by cycling the indexer once and then
 	// pausing to let the shooter motor spin back up to full speed.
 	void DoAutonomousShootOneDisk()
 	{
-		// Unless the caller does an update, we will still see the openEvent
-		// if we call getEvent unless we do an update here
+		// We need to do an update here in order to see any changes
+		// (autonomous mode is not a loop like teleop mode).
 		indexerSwitch.Update();
 
 		indexerMotor.Set(INDEXER_FWD);
 
-		// Wait for the indexer to cycle once
+		// Wait for the indexer to cycle once (to prevent an unlikely but
+		// possible infinite loop that would prevent us from going into
+		// telop mode, we also exit the loop if autonomous mode ends)
 		while (IsAutonomous() && indexerSwitch.GetEvent() != kEventOpened)
 		{
 			Wait(0.02);
@@ -189,10 +194,13 @@ public:
 		
 		shooterMotor.Set(0.0f);
 
-		// Move the robot to a position closer to out feeder station 
-		//		DoAutonomousMoveStep(&m_autoForward[0], "Backing up...");
+		// Move the robot to a position closer to our feeder station 
+		// (commented out until we get a chance to actually try it)
+//		DoAutonomousMoveStep(&m_autoForward[0], "Backing up...");
 	}
 	
+	// Driver inputs are joystick one and a single button on joystick two
+	// which shifts the gearbox (press and hold)
 	void HandleDriverInputsManual(void)
 	{
 		myRobot.ArcadeDrive(stick.GetX(), stick.GetY());
@@ -216,11 +224,16 @@ public:
 		}
 	}
 	
+	// The arm inputs are:
+	// - the left gamepad joystick for continuous climb and descend, 
+	// - dpad up and down to climb and descend incrementaly
+	// - two buttons to lock the claws
+	// - two buttons to release the claws
 	void HandleArmInputs(void)
 	{
 		if (!m_jogTimerRunning)
 		{
-			// Climb!
+			// Climb
 			if (gamepad.GetLeftY() < -0.1)
 			{
 				if (potentiometer.GetVoltage() > CLIMB_LIMIT)
@@ -289,22 +302,31 @@ public:
 
 		if (gamepad.GetEvent(BUTTON_GREEN_CLAW_LOCKED) == kEventClosed)
 		{
+			// lock the green claw
 			greenClaw.Set(DoubleSolenoid::kForward);
 		}
 		else if (gamepad.GetEvent(BUTTON_GREEN_CLAW_UNLOCKED) == kEventClosed)
 		{
+			// unlock the green claw
 			greenClaw.Set(DoubleSolenoid::kReverse);
 		}
 		else if (gamepad.GetEvent(BUTTON_YELLOW_CLAW_LOCKED) == kEventClosed)
 		{
+			// lock the yellow claw
 			yellowClaw.Set(DoubleSolenoid::kForward);
 		}
 		else if (gamepad.GetEvent(BUTTON_YELLOW_CLAW_UNLOCKED) == kEventClosed)
 		{
+			// unlock the yellow claw
 			yellowClaw.Set(DoubleSolenoid::kReverse);
 		}
 	}
 	
+	// The shooter controls are:
+	// - a gamepad button to start/stop the shooter motor
+	// - a gamepad button to start the indexer (the indexer stops
+	//   when the indexer switch opens (it is normally closed) when
+	//   the an index cycle is complete)
 	void HandleShooterInputs()
 	{
 		if (indexerSwitch.GetEvent() == kEventOpened)
@@ -332,6 +354,10 @@ public:
 		}
 	}
 	
+	// The reset button is intended to put the robot into a known
+	// (and safe) state. It should not be required outside of debugging
+	// new code but, as previous competitions have shown, you never know
+	// when you might need it do to sensor switch failure, etc.)
 	void HandleResetButton(void)
 	{
 		if (gamepad.GetEvent(BUTTON_STOP_ALL) == kEventClosed)
@@ -351,7 +377,11 @@ public:
 
 	void UpdateStatusDisplays(void)
 	{
-		// Joystick values
+		// Joystick values (currently commented out to reduce network traffic and because
+		// this is really nothing more than dashboard "bling". Although not certain, it 
+		// is highly suspected that our "dead robot" failures at the first competition
+		// were caused by overloading of the network tables communication path [which the
+		// SmartDashboard uses]).
 //		SmartDashboard::PutNumber("stickX", stick.GetX());
 //		SmartDashboard::PutNumber("stickY", stick.GetY());
 //		SmartDashboard::PutBoolean("shift", stick2.GetState(BUTTON_SHIFT) ? kStateClosed : kStateOpen);
@@ -361,11 +391,14 @@ public:
 		SmartDashboard::PutNumber("shooterMotor", shooterMotor.Get());
 		SmartDashboard::PutNumber("indexerMotor", indexerMotor.Get());
 
-		// Misc Motor Values
+		// Misc Motor Values (as with the joystick values above, commented out to reduce network
+		// traffic to the dashboard)
+		
 //		SmartDashboard::PutNumber("armMotor", armMotor.Get());
 
 		// Arm position via potentiometer voltage (2.5 volts is center position)
 		float potVal = potentiometer.GetVoltage();
+		
 		// Checks if there has been a significant enough change in the value of
 		// the arm potentiometer to warrant sending the data to the dashboard.
 		if (absolute((m_previousPot - potVal)) > POT_EPSILON)
@@ -381,7 +414,8 @@ public:
 		// Pneumatic shifter count
 		SmartDashboard::PutNumber("Shift Count", m_shiftCount);
 		
-		// State viariables
+		// State viariables. This is basically debug and is commented out, once again to reduce
+		// network traffic.
 //		dsLCD->PrintfLine(DriverStationLCD::kUser_Line6, "SMR: %s JTR: %s",
 //			m_shooterMotorRunning ? "T" : "F",   
 //			m_jogTimerRunning ? "T" : "F");
@@ -389,6 +423,10 @@ public:
 	
 	void OperatorControl(void)
 	{
+		// As counter intuitive as this is, it was suggested to us by experts at the
+		// first competition after we had a number of "dead robot" incidents during
+		// the competition. There are emergency stop buttons available to the drive team
+		// and to the field managers.
 		myRobot.SetSafetyEnabled(false);
 		
 		gamepad.EnableButton(BUTTON_GREEN_CLAW_LOCKED);
@@ -431,10 +469,12 @@ public:
 			HandleResetButton();
 			UpdateStatusDisplays();
 			
-			dsLCD->PrintfLine(DriverStationLCD::kUser_Line4, "G: %s", (GREEN_CLAW_LOCK_STATE ? "Locked" : "Unlocked"));
-			dsLCD->PrintfLine(DriverStationLCD::kUser_Line5, "Y: %s", (YELLOW_CLAW_LOCK_STATE ? "Locked" : "Unlocked"));
-			
-			dsLCD->UpdateLCD();
+			// No longer needed as this information is output to the drive station
+			// via the SmartDashboard
+//			dsLCD->PrintfLine(DriverStationLCD::kUser_Line4, "G: %s", (GREEN_CLAW_LOCK_STATE ? "Locked" : "Unlocked"));
+//			dsLCD->PrintfLine(DriverStationLCD::kUser_Line5, "Y: %s", (YELLOW_CLAW_LOCK_STATE ? "Locked" : "Unlocked"));
+//			
+//			dsLCD->UpdateLCD();
 			Wait(0.005);				// wait for a motor update time
 		}
 	}
